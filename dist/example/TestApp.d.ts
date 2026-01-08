0038e66030df6c61c8510a0511f2ddca03b8617f
gudmunduregill <geb.2991@gmail.com>
2026-01-08 13:58:39 +0000

    onmore

--- dist/example/TestApp.d.ts
+++
@@ -1,2 +0,0 @@
-export default function TestApp(): import("react/jsx-runtime").JSX.Element;
-//# sourceMappingURL=TestApp.d.ts.map
--- dist/integrations/index.d.ts
+++
@@ -1,7 +0,0 @@
-export type { Platform, TrackingConfig, BaseTrackEvent, SinnaBookingEvent, SinnaBookingEventMap, DineoutReservationEvent, DineoutReservationEventMap, TrackableEvent, TrackableEventMap, EventMapping, TrackMethod, EventMap, TrackingEventFunction, } from './types';
-export { trackLog, currentVersion, verbose, detectPlatform, EVENT_MAP, mapEventName, DO_TRACKING_INTEGRATIONS, clearIntegrations, addIntegration, injectScriptOnce, mapFacebookToGA4, } from './types';
-export { initFacebookPixel, trackToFBPixel, isFBPixelInitialized, } from './facebook';
-export { initGA4, initGTM, trackToGA4, trackToGTM, isGA4Initialized, isGTMInitialized, } from './google';
-export type { BookingFunnelEvent, InitMixpanelOptions } from './mixpanel';
-export { initMixpanel, trackToMixpanel, trackBookingEvent, identifyUser, trackMixpanelEvent, resetMixpanel, isMixpanelInitialized, getCurrentPlatform, getCurrentCompanyId, } from './mixpanel';
-//# sourceMappingURL=index.d.ts.map
